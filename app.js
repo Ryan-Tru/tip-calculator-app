f461225dd210e66f23c473607930b924e17f121d
Ryan Truong <ryan.truong2304@gmail.com>
2022-03-26 18:01:14 +1100

    declared variables

---
+++ app.js
@@ -0,0 +1,4 @@
+const billAmt = document.querySelector('#bill-amt')
+const tipPercentageBtn = document.querySelectorAll('[data-tip]');
+const customTip = document.querySelector('.custom');
+const peopleAmt = document.querySelector('#people-amt');
