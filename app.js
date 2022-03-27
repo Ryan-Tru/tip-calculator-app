1efc1a476f98a5327696c06368acb755ef240825
Ryan Truong <ryan.truong2304@gmail.com>
2022-03-27 16:33:39 +1100

    now appends the totals

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const billAmt = document.querySelector('#bill-amt')
 const tipPercentageBtn = document.querySelectorAll('[data-tip]');
 const customTip = document.querySelector('.custom');
 const peopleAmt = document.querySelector('#people-amt');
-const tipTotal = document.querySelector('.tip-total');
-const totalAmt = document.querySelector('.total-amt');
+const tipTotalText = document.querySelector('.tip-total');
+const totalAmtText = document.querySelector('.total-amt');
 
-let tipPercent;
-let bill;
-let people;
+let tipPercent = 0;
+let bill = 0;
+let people = 0;
 
 //Changes button color when selected, using if else statement to remove and add class
 tipPercentageBtn.forEach(button => {
@@ -26,39 +26,37 @@ tipPercentageBtn.forEach(button => {
             twentyfivePercent.classList.remove('percent-btn-selected');
             fiftyPercent.classList.remove('percent-btn-selected');
 
-            tipPercent = fivePercent.getAttribute('data-tip');
-
+            tipPercent = parseFloat(fivePercent.getAttribute('data-tip'));
         } else if(button.classList.contains('ten-percent')) {
             fivePercent.classList.remove('percent-btn-selected');
             fifteenPercent.classList.remove('percent-btn-selected');
             twentyfivePercent.classList.remove('percent-btn-selected');
             fiftyPercent.classList.remove('percent-btn-selected');
 
-            tipPercent = tenPercent.getAttribute('data-tip');
+            tipPercent = parseFloat(tenPercent.getAttribute('data-tip'));
         } else if(button.classList.contains('fifteen-percent')) {
             fivePercent.classList.remove('percent-btn-selected');
             tenPercent.classList.remove('percent-btn-selected');
             twentyfivePercent.classList.remove('percent-btn-selected');
             fiftyPercent.classList.remove('percent-btn-selected');
 
-            tipPercent = fifteenPercent.getAttribute('data-tip');
+            tipPercent = parseFloat(fifteenPercent.getAttribute('data-tip'));
         } else if(button.classList.contains('twentyfive-percent')) {
             fivePercent.classList.remove('percent-btn-selected');
             tenPercent.classList.remove('percent-btn-selected');
             fifteenPercent.classList.remove('percent-btn-selected');
             fiftyPercent.classList.remove('percent-btn-selected');
 
-            tipPercent = twentyfivePercent.getAttribute('data-tip');
+            tipPercent = parseFloat(twentyfivePercent.getAttribute('data-tip'));
         } else if(button.classList.contains('fifty-percent')) {
             fivePercent.classList.remove('percent-btn-selected');
             tenPercent.classList.remove('percent-btn-selected');
             fifteenPercent.classList.remove('percent-btn-selected');
             twentyfivePercent.classList.remove('percent-btn-selected');
 
-            tipPercent = fiftyPercent.getAttribute('data-tip');
+            tipPercent = parseFloat(fiftyPercent.getAttribute('data-tip'));
         }
-
-        console.log(tipPercent);
+        appendTotals();
     });
 });
 
@@ -72,13 +70,35 @@ customTip.addEventListener('click', () => {
 });
 
 //Gets percentage
-customTip.addEventListener('input', () => tipPercent = customTip.value / 100);
+customTip.addEventListener('input', () => {
+    tipPercent = parseFloat(customTip.value / 100);
+    appendTotals();
+});
 
 //Gets bill amount
-billAmt.addEventListener('input', () => bill = billAmt.value);
+billAmt.addEventListener('input', () => {
+    bill = parseFloat(billAmt.value);
+    appendTotals();
+});
+
 
 //Gets people amount
-peopleAmt.addEventListener('input', () => people = peopleAmt.value);
+peopleAmt.addEventListener('input', () => {
+    people = parseFloat(peopleAmt.value);
+    appendTotals();
+});
+
+function appendTotals() {  
+    let tipTotal = bill * tipPercent / people;
+    let total = bill / people;
+    if(people >= 1) {
+        tipTotalText.innerHTML = `\$${tipTotal.toFixed(2)}`;
+        totalAmtText.innerHTML = `\$${total.toFixed(2)}`;
+    } else {
+        tipTotalText.innerHTML = `\$0.00`;
+        totalAmtText.innerHTML = `\$0.00`;
+    }
+}
 
 
 
